58fb53794721c01213e00143f9757eadef26ccfd
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2024-11-08 14:46:24 +0100

    Fix code errors and remove unnecessary comments
     Date:      Fri Nov 8 14:46:24 2024 +0100

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,37 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		// Lecture des symptômes à partir du fichier "symptoms.txt"
+		ReadSymptomDataFromFile symptomDataReader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = symptomDataReader.GetSymptoms();
 
-			line = reader.readLine();	// get another symptom
+		// Comptage des occurrences de chaque symptôme
+		Map<String, Integer> counters = new HashMap<String, Integer>();
+		symptoms.forEach(symptom -> {
+			// Si le symptôme est déjà présent dans la map, on incrémente son compteur,
+			// sinon on initialise à 1
+			counters.put(symptom, counters.getOrDefault(symptom, 0) + 1);
+		});
+
+		Map<String, Integer> sortedCounters = new TreeMap<>(counters);
+
+		// Écriture des résultats dans un fichier "result.out"
+		try (FileWriter writer = new FileWriter("result.txt")) {
+			// Boucle pour écrire chaque symptôme et son nombre d'occurrences
+			for (Map.Entry<String, Integer> entry : sortedCounters.entrySet()) {
+				writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 }
